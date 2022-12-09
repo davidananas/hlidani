e3f8a055713ddd25b7c9bc1f4659aba2e4e10e6e
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-09 17:47:01 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 input.onPinReleased(TouchPin.P0, function () {
+    houkání = 0
     basic.showIcon(IconNames.Angry)
-    for (let index = 0; index < 100; index++) {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    while (houkání == 0) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 1, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -12,6 +13,7 @@ input.onButtonPressed(Button.B, function () {
     Vstup = "" + Vstup + "B"
 })
 let Vstup = ""
+let houkání = 0
 let Heslo = "ABAB"
 basic.forever(function () {
     if (Heslo == Vstup) {
