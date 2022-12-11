e074ce53240510d16364f9333e0efcc9c06f91ab
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-11 07:31:32 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     Vstup = "" + Vstup + "A"
+    basic.showString("A")
 })
 input.onButtonPressed(Button.B, function () {
     Vstup = "" + Vstup + "B"
+    basic.showString("B")
 })
 input.onGesture(Gesture.Shake, function () {
     houkání = 0
@@ -11,6 +13,10 @@ input.onGesture(Gesture.Shake, function () {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 1, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
         music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Vstup = ""
+    basic.showString("DELETED")
 })
 let houkání = 0
 let Vstup = ""
